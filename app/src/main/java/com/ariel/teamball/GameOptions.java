444d8e7e7dfac659a48d63040e3e9a238863688e
OfirOvadia96 <ofirrr999@gmail.com>
2021-11-18 16:51:55 +0200

    ofir

--- app/src/main/java/com/ariel/teamball/GameOptions.java
+++ app/src/main/java/com/ariel/teamball/GameOptions.java
@@ -10,8 +10,8 @@ import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class GameOptions extends AppCompatActivity {
-
-    Button mLogoutBtn, mMyProfileBtn;
+    /* Setting Buttons */
+    Button mLogoutBtn, SoccerBtn, BasketBallBtn, TennisBtn, TableTennisBtn, HandBallBtn, VolleyBallBtn, DogeBallBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +19,17 @@ public class GameOptions extends AppCompatActivity {
         setContentView(R.layout.activity_game_options);
         getSupportActionBar().hide();
 
+        /* Catches them */
         mLogoutBtn = findViewById(R.id.LogoutBtn);
-        mMyProfileBtn = findViewById(R.id.ProfileBtn);
+        SoccerBtn = findViewById(R.id.SoccerBtn);
+        BasketBallBtn = findViewById(R.id.BasketBallBtn);
+        TennisBtn = findViewById(R.id.TenisBtn);
+        TableTennisBtn = findViewById(R.id.TableTennisBtn);
+        HandBallBtn = findViewById(R.id.HandBallBtn);
+        VolleyBallBtn = findViewById(R.id.VolleyBallBtn);
+        DogeBallBtn = findViewById(R.id.DogeBallBtn);
 
+        /* What each button does */
         mLogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,13 +39,54 @@ public class GameOptions extends AppCompatActivity {
             }
         });
 
-        mMyProfileBtn.setOnClickListener(new View.OnClickListener() {
+        SoccerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MyProfile.class));
-                finish();
+
             }
         });
+        BasketBallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TennisBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TableTennisBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        HandBallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        VolleyBallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        DogeBallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 
